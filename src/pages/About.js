00e24881b8854b64072becb01342854bb8d3cdce
Yifan Wang <yifan.wang0422@gmail.com>
2022-06-15 20:16:09 -0400

    finished everything up until add deleteTodo as new property

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,37 @@
-import React, { Component } from "react";
-import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
+import React, { Component } from 'react';
 
+import "./About.css";
+import YifanWang_Headshot from "../assets/YifanWang_Headshot.jpeg";
+  
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
-        <div class="split left">
+        <div className="split left">
           <div className="centered">
-            <img
+            <img 
               className="profile_image"
-              src={profile_pic}
+              src={YifanWang_Headshot}
               alt="Profile Pic"
-            ></img>
+              ></img>
           </div>
         </div>
         <div className="split right">
           <div className="centered">
-            <div className="name_title">Your Name</div>
+            <div className="name_title">Yifan Wang</div>
             <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
+              Hello! My name is Yifan Wang, and I am a rising junior at MIT majoring in Computation and Cognition.
+              I am participating in IBM Accelerate as part of the software track! This summer, along with this program, 
+              I am also interning at Twitter as a software engineer intern. I am very interested in computer science, 
+              but in particular I would love to learn more about machine learning and artificial intelligence.
+
+              During my free time, I like to watch TV shows, do arts and crafts, listen to music, and explore different 
+              places (and foods!) with friends. Also, a fun fact about me: I have never read nor watched any of the Harry
+              Potter series.
             </div>
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
